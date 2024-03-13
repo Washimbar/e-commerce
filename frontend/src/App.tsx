9a3c167cbb2181a8188cdfbd4c73ef3b1a081ed6
WashimBar <washimbari0001@gmail.com>
2024-03-14 04:23:04 +0530

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import CakeView from './features/cake/CakeView'
-import './App.css'
-import MyComponent from './features/api/MyComponent';
+
 const App = () => {
   return (
-    <div>
-      <MyComponent />
-    </div>
+    <div>App</div>
   )
 }
 
